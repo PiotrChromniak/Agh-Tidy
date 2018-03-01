5d916786f6b943ae3b434e70e152eb17a34e7f1a
PiotrChromniak <piotr.chromniak@gmail.com>
2018-03-01 21:17:43 +0100

    Complete implementation of reformatter

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Agh-Tidy
 ics. calendar reformatter for AGH
+PoC Rust code
+inspired by tomblachut's
+https://github.com/tomblachut/AGH-schedule-optimizer
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+extern crate regex;
+use std::error::Error;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use regex::Regex;
+
+fn main() {
+    if let Some(args1) = env::args().nth(1) {
+        println!("The first argument is {}", &args1);
+        let dict = [
+            ("Zaawansowane techniki internetowe", "ZTI"),
+            ("Techniki mikroprocesorowe", "Mikroprocesory"),
+            ("Eksploracja danych", "ED"),
+            ("Modelowanie procesów fizycznych", "MPF"),
+            ("Systemy równoległe i rozproszone", "SRiR"),
+            ("Fizykochemia procesów", "Fizykochemia"),
+            ("Fizyka III", "Fizyka")
+        ];
+        let path = Path::new(&args1);
+        let mut file = match File::open(&path){
+            Err(why) => panic!("couldn't open {}: {}", path.display(), why.description()),
+            Ok(file) => file,
+        };
+        let mut content = String::new();
+        match file.read_to_string(&mut content) {
+            Err(why) => panic!("couldn't read {}: {}", path.display(), why.description()),
+            Ok(_) => println!("succesfully read content"),
+        };
+
+        for &(key, rep) in dict.into_iter() {
+            content = content.replace(key, rep);
+        }
+
+        let trim_unnecessary_word = Regex::new(r"Sala:\s?").unwrap();
+        let trim_teacher = Regex::new(r"Prowadzący: (?:mgr |inż. |prof. |dr |hab. )*(.*?)").unwrap();
+        let reformat_location = Regex::new(r"(?P<letter>[a-zA-Z])-(?P<num>\d{1,2}) - s. (?P<class_num>\d{1,3}\w?)").unwrap();
+
+        let clean = trim_teacher.replace_all(&content, "");
+        let clean2 = reformat_location.replace_all(&clean, "$letter-$num $class_num");
+        let clean3 = trim_unnecessary_word.replace_all(&clean2, "");
+
+        let new_path = Path::new("plan_zajec_plus.ics");
+        let mut new_file = match File::create(&new_path){
+            Err(why) => panic!("couldn't create output file {} {}", new_path.display(), why.description()),
+            Ok(created_file) => created_file,
+        };
+        match new_file.write_all(clean3.as_bytes()) {
+        Err(why) => {
+            panic!("couldn't write: {}", why.description())
+        },
+        Ok(_) => println!("successfully wrote to" ),
+    }
+        println!("File succesfuly reformated");
+    } else {
+        println!("No file specificated");
+    }
+}
